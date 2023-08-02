3c0e41303dfcc70994e2d3516be1e965c0190fcd
Anne-flor <annygabrielly730@gmail.com>
2023-08-02 18:10:55 -0300

    Adicionando pontos ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,6 +6,7 @@ print("*********************************")
 
 numero_secreto = (random.randrange(1, 101))
 total_de_tentativas = 0
+pontos = 1000
 
 print('Qual o nível de dificuldade ?')
 print('(1) Fácil (2)Médio (3)Difícil')
@@ -21,6 +22,7 @@ else:
 
 for rodada in range(1, total_de_tentativas + 1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
+
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
@@ -34,12 +36,13 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {}!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou!, seu chute foi maior que o número secreto")
         elif(menor):
             print('Você errou, seu chute foi menor que o número secreto')
-
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos 
 print("Fim do jogo")
