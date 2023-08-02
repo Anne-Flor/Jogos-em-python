5fc18817f0bc653362acb0463e8644dac745f6de
Anne-flor <annygabrielly730@gmail.com>
2023-08-02 17:41:47 -0300

    Limitando as jogadas do usuário

--- adivinhacao.py
+++ adivinhacao.py
@@ -11,12 +11,17 @@ for rodada in range(1, total_de_tentativas + 1):
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
+    if(chute < 1 or chute > 100):
+        print('Você deve digitar um número entre 1 e 100')
+        continue
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou!, seu chute foi maior que o número secreto")
