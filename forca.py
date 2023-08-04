348464515f9e1431db99cbb7bdcfa68ea7683f05
Anne-flor <annygabrielly730@gmail.com>
2023-08-04 17:07:26 -0300

    Deixando o código mais organizado, utilizando funções

--- forca.py
+++ forca.py
@@ -1,23 +1,12 @@
 import random
+
 def jogar():
 
-    print('****************************************')
-    print('BEM VINDO AO JOGO DA FORCA')
-    print('****************************************')
+    imprime_mensagem_abertura()
 
-    arquivo = open("/home/flor/AluraVerso/Jogos-em-python/Palavras.txt", "r")
-    palavras = []
+    palavra_secreta = carrega_palavra_secreta()
 
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ['_' for letra in palavra_secreta]
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -50,6 +39,30 @@ def jogar():
     else:
         print('Você perdeu')
     print('Fim do jogo')
+
+def imprime_mensagem_abertura():
+
+    print('****************************************')
+    print('BEM VINDO AO JOGO DA FORCA')
+    print('****************************************')
+
+def carrega_palavra_secreta():
+
+    arquivo = open("/home/flor/AluraVerso/Jogos-em-python/Palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ['_' for letra in palavra]
 
 if(__name__ == '__main__'):
     jogar()
